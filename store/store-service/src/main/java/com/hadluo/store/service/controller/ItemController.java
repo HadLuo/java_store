253ca053b858afadef3da8013ae762102e8e8ac1
hadluo <8000947+hadluo@user.noreply.gitee.com>
2020-12-15 13:21:31 +0800

    增加注释

--- store/store-service/src/main/java/com/hadluo/store/service/controller/ItemController.java
+++ store/store-service/src/main/java/com/hadluo/store/service/controller/ItemController.java
@@ -38,67 +38,103 @@ public class ItemController {
 	MainConfigMapper mainConfigMapper;
 	@Autowired
 	MainBannerMapper mainBannerMapper;
-	
-	
+
+	/***
+	 * 根据宠物类别查询宠物文章
+	 * 
+	 * @param typeId
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/selectByTypes")
-	public Result<List<Item>> selectByTypes(@RequestParam(name = "typeId",defaultValue = "1") Integer typeId) {
+	public Result<List<Item>> selectByTypes(@RequestParam(name = "typeId", defaultValue = "1") Integer typeId) {
 		ItemSelect itemSelect = new ItemSelect();
 		itemSelect.setType(typeId);
 		itemSelect.setPageSize(Integer.MAX_VALUE);
 		return Result.ok(itemMapper.selectByPage(itemSelect));
 	}
 
-	
+	/***
+	 * 根据文章id 查询 宠物详情
+	 * 
+	 * @param id
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/selectOne")
-	public Result<Item> selectOne(@RequestParam(name = "id",defaultValue = "1") Integer id) {
-		Result<Item> r =  Result.ok(itemMapper.selectByPrimaryKey(id));
+	public Result<Item> selectOne(@RequestParam(name = "id", defaultValue = "1") Integer id) {
+		Result<Item> r = Result.ok(itemMapper.selectByPrimaryKey(id));
 		Logs.e(getClass(), "JokeController selectOne req >> id=" + id + " , ret = " + JSON.toJSONString(r));
-		return r ;
+		return r;
 	}
 
+	/***
+	 * 查询宠物类别
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/types")
 	public Result<List<ItemType>> types() {
-		List<ItemType> types = itemTypeMapper.selectSortByPage() ;
-		Logs.e(getClass(), "types>>" + JSON.toJSONString(types) );
+		List<ItemType> types = itemTypeMapper.selectSortByPage();
+		Logs.e(getClass(), "types>>" + JSON.toJSONString(types));
 		return Result.ok(types);
 	}
-	
+
+	/***
+	 * 根据类别id查询类别详情
+	 * 
+	 * @param id
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/type")
-	public Result<ItemType> type(@RequestParam(name = "id",defaultValue = "1") Integer id) {
+	public Result<ItemType> type(@RequestParam(name = "id", defaultValue = "1") Integer id) {
 		return Result.ok(itemTypeMapper.selectByPrimaryKey(id));
 	}
-	
+
+	/***
+	 * 首页的 宠物推荐文章
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/mainitems")
 	public Result<List<Item>> mainItems() {
 		List<MainItems> items = mainItemsMapper.selectSortByPage();
 		List<Item> results = Lists.newArrayList();
-		for(MainItems item : items) {
+		for (MainItems item : items) {
 			results.add(itemMapper.selectByPrimaryKey(item.getItemId()));
 		}
-		Logs.e(getClass(), "mainItems>>" + JSON.toJSONString(results) );
+		Logs.e(getClass(), "mainItems>>" + JSON.toJSONString(results));
 		return Result.ok(results);
 	}
-	
+
+	/***
+	 * 一些主配置 ,（联系人，二维码等等）
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/maincfg")
 	public Result<MainConfig> mainConfig() {
 		MainConfig record = new MainConfig();
 		record.setIsDelete(0);
-		MainConfig cfg = mainConfigMapper.selectOne(record) ;
-		Logs.e(getClass(), "mainConfig>>" + JSON.toJSONString(cfg) );
+		MainConfig cfg = mainConfigMapper.selectOne(record);
+		Logs.e(getClass(), "mainConfig>>" + JSON.toJSONString(cfg));
 		return Result.ok(cfg);
 	}
-	
+
+	/***
+	 * 首页 banner图
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@GetMapping("/banners")
 	public Result<List<MainBanner>> banners() {
 		List<MainBanner> datas = mainBannerMapper.selectSortByPage();
-		Logs.e(getClass(), "banners>>" + JSON.toJSONString(datas) );
+		Logs.e(getClass(), "banners>>" + JSON.toJSONString(datas));
 		return Result.ok(datas);
 	}
 
